08268e0806efb390ad6132f7514074abadd3809d
Pamanya <110141549+Pamanya@users.noreply.github.com>
2023-04-10 20:15:43 -0300

    Update tu-archivo.js

--- tu-archivo.js
+++ tu-archivo.js
@@ -1,80 +1,55 @@
-$(document).ready(function() {
-  const player = new Plyr('#player');
-  const playlist = $('#playlist');
-  const nextChapter = $('#next-chapter');
-  const countdown = $('#countdown');
-  const videos = $('#playlist a');
-  let currentVideo = 0;
-  
-  // Cargar el primer video de la lista
-  changeVideo(videos.eq(currentVideo));
-  
-  // Al hacer clic en un elemento de la lista, cambiar el video
-  videos.on('click', function(e) {
-    e.preventDefault();
-    changeVideo($(this));
-  });
-  
-  // Función para cambiar el video
-  function changeVideo(video) {
-    currentVideo = video.parent().index();
-    playlist.find('.active').removeClass('active');
-    video.parent().addClass('active');
-    const src = video.data('src');
-    player.source = {
-      type: 'video',
-      sources: [
-        {
-          src: src,
-          type: 'video/mp4',
-        },
-      ],
-    };
-    player.play();
-    nextChapter.removeClass('show');
-    countdown.removeClass('show');
+// array de videos y títulos
+var playlist = [
+  {
+    title: "Video 1",
+    url: "https://example.com/video1.mp4"
+  },
+  {
+    title: "Video 2",
+    url: "https://example.com/video2.mp4"
+  },
+  {
+    title: "Video 3",
+    url: "https://example.com/video3.mp4"
   }
-  
-  // Al finalizar el video, mostrar el mensaje de "Capítulo siguiente" y cuenta regresiva
-  player.on('ended', function() {
-    nextChapter.addClass('show');
-    let count = 10;
-    const countdownText = $('<span/>', {
-      text: count,
-      class: 'countdown-text'
-    });
-    countdown.empty().append(countdownText);
-    countdown.addClass('show');
-    const timer = setInterval(function() {
-      count--;
-      countdownText.text(count);
-      if (count == 0) {
-        clearInterval(timer);
-        nextVideo();
+];
+
+// inicializar video.js
+var player = videojs('my-video');
+
+// inicializar playlist
+var playlistMenu = player.playlistUi();
+
+// agregar videos a la lista de reproducción
+for (var i = 0; i < playlist.length; i++) {
+  player.playlist.push(playlist[i]);
+}
+
+// reproducir el primer video de la lista de reproducción
+player.playlist.first();
+
+// mostrar el título del video en la lista de reproducción cuando se selecciona
+player.on('playlistitem', function() {
+  var item = player.playlist.currentItem();
+  var title = playlist[item].title;
+  playlistMenu.currentItem(item);
+  playlistMenu.currentItem().el().firstChild.innerHTML = title;
+});
+
+// mostrar el mensaje "Capitulo siguiente" y la cuenta regresiva después de reproducir un video
+player.on('ended', function() {
+  var nextItem = player.playlist.next();
+  if (nextItem) {
+    player.playlist.next();
+    var countdown = 10;
+    var countdownInterval = setInterval(function() {
+      if (countdown === 0) {
+        clearInterval(countdownInterval);
+        player.playlist.next();
+      } else {
+        console.log("Capitulo siguiente en " + countdown);
+        countdown--;
       }
     }, 1000);
-  });
-  
-  // Cambiar al siguiente video de la lista
-  function nextVideo() {
-    currentVideo++;
-    if (currentVideo >= videos.length) {
-      currentVideo = 0;
-    }
-    changeVideo(videos.eq(currentVideo));
   }
-  
-  // Iniciar la reproducción automática del primer video
-  player.autoplay = true;
-  
-  // Función para iniciar la reproducción del video
-  function playVideo() {
-    const video = videos.eq(currentVideo);
-    changeVideo(video);
-  }
-  
-  // Al hacer clic en el botón de "Capítulo siguiente", cambiar al siguiente video
-  nextChapter.on('click', function() {
-    nextVideo();
-  });
 });
