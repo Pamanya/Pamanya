c28e644fc261db21c1bd32a5d9656bd3a097d5a2
Pamanya <110141549+Pamanya@users.noreply.github.com>
2023-04-10 22:42:45 -0300

    Update tu-archivo.js

--- tu-archivo.js
+++ tu-archivo.js
@@ -1,55 +1,80 @@
-// array de videos y títulos
+// Array of video sources to be played
 var playlist = [
   {
-    title: "Video 1",
-    url: "https://example.com/video1.mp4"
+    name: "Episode 1",
+    sources: [
+      { src: "https://example.com/episode1.mp4", type: "video/mp4" },
+      { src: "https://example.com/episode1.webm", type: "video/webm" }
+    ]
   },
   {
-    title: "Video 2",
-    url: "https://example.com/video2.mp4"
+    name: "Episode 2",
+    sources: [
+      { src: "https://example.com/episode2.mp4", type: "video/mp4" },
+      { src: "https://example.com/episode2.webm", type: "video/webm" }
+    ]
   },
   {
-    title: "Video 3",
-    url: "https://example.com/video3.mp4"
+    name: "Episode 3",
+    sources: [
+      { src: "https://example.com/episode3.mp4", type: "video/mp4" },
+      { src: "https://example.com/episode3.webm", type: "video/webm" }
+    ]
   }
 ];
 
-// inicializar video.js
-var player = videojs('my-video');
+// Video player initialization
+var player = videojs("my-video");
 
-// inicializar playlist
-var playlistMenu = player.playlistUi();
-
-// agregar videos a la lista de reproducción
-for (var i = 0; i < playlist.length; i++) {
-  player.playlist.push(playlist[i]);
+// Function to play video from playlist
+function playVideo(index) {
+  player.pause();
+  player.src(playlist[index].sources);
+  player.load();
+  player.play();
+  setActive(index);
+  var videoTitle = document.getElementById("video-title");
+  videoTitle.innerHTML = playlist[index].name;
+  player.on("ended", function() {
+    var nextIndex = (index + 1) % playlist.length;
+    videoTitle.innerHTML = "Next Episode: " + playlist[nextIndex].name;
+    setTimeout(function() {
+      videoTitle.innerHTML = playlist[nextIndex].name;
+      playVideo(nextIndex);
+    }, 10000); // 10 second countdown
+  });
 }
 
-// reproducir el primer video de la lista de reproducción
-player.playlist.first();
-
-// mostrar el título del video en la lista de reproducción cuando se selecciona
-player.on('playlistitem', function() {
-  var item = player.playlist.currentItem();
-  var title = playlist[item].title;
-  playlistMenu.currentItem(item);
-  playlistMenu.currentItem().el().firstChild.innerHTML = title;
-});
-
-// mostrar el mensaje "Capitulo siguiente" y la cuenta regresiva después de reproducir un video
-player.on('ended', function() {
-  var nextItem = player.playlist.next();
-  if (nextItem) {
-    player.playlist.next();
-    var countdown = 10;
-    var countdownInterval = setInterval(function() {
-      if (countdown === 0) {
-        clearInterval(countdownInterval);
-        player.playlist.next();
-      } else {
-        console.log("Capitulo siguiente en " + countdown);
-        countdown--;
-      }
-    }, 1000);
+// Function to set active class for current playlist item
+function setActive(index) {
+  var items = document.getElementsByClassName("vjs-playlist-item");
+  for (var i = 0; i < items.length; i++) {
+    items[i].classList.remove("active");
   }
-});
+  items[index].classList.add("active");
+}
+
+// Initialize playlist
+var playlistItems = [];
+for (var i = 0; i < playlist.length; i++) {
+  var item = {
+    sources: playlist[i].sources,
+    poster: playlist[i].poster,
+    name: playlist[i].name
+  };
+  playlistItems.push(item);
+}
+player.playlist(playlistItems);
+
+// Play first video on page load
+playVideo(0);
+
+// Set click listeners for playlist items
+var items = document.getElementsByClassName("vjs-playlist-item");
+for (var i = 0; i < items.length; i++) {
+  (function(index) {
+    items[i].addEventListener("click", function() {
+      playVideo(index);
+    });
+  })(i);
+}
